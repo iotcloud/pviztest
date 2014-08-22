7faa29afc35839f5f990b848d319cd1aef7f6250
Chathuri Wimalasena <kamalasini@gmail.com>
2014-08-21 21:14:54 -0400

    getting the plot to working

--- src/main/java/cgl/pviz/test/ActiveMQSender.java
+++ src/main/java/cgl/pviz/test/ActiveMQSender.java
@@ -16,7 +16,7 @@ public class ActiveMQSender {
     public void run() {
         try {
             // Create a ConnectionFactory
-            ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost");
+            ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://149.165.159.16:61616");
 
             // Create a Connection
             Connection connection = connectionFactory.createConnection();
@@ -25,7 +25,7 @@ public class ActiveMQSender {
             // Create a Session
             Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
             // Create the destination (Topic or Queue)
-            Destination destination = session.createQueue("TEST.FOO");
+            Destination destination = session.createTopic("topic2");
             // Create a MessageProducer from the Session to the Topic or Queue
             MessageProducer producer = session.createProducer(destination);
             producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
@@ -33,8 +33,24 @@ public class ActiveMQSender {
             // Create a messages
 
             while (run) {
-                Message.PvizMessage m = Message.PvizMessage.getDefaultInstance();
 
+
+                Message.PvizMessage m;
+                Message.PvizMessage.Builder build = Message.PvizMessage.newBuilder();
+                build.setStepid(3);
+                build.setTimestamp(System.currentTimeMillis());
+                build.setType(Message.PvizMessage.mtype.DATA);
+
+                for (int i = 0; i < 100; i++) {
+                    build.addLabels(i);
+                    double x = Math.random() * 100;
+                    double y = Math.random() * 100;
+                    double z = Math.random() * 100;
+                    build.addPositions(Message.Position.newBuilder().setX(x).setY(y).setZ(z));
+                }
+                m = build.build();
+                BytesMessage message = session.createBytesMessage();
+                message.writeBytes(m.toByteArray());
                 producer.send(message);
             }
             // Clean up
@@ -45,5 +61,10 @@ public class ActiveMQSender {
             System.out.println("Caught: " + e);
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        ActiveMQSender sender = new ActiveMQSender();
+        sender.run();
     }
 }
